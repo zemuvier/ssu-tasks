7bb2f06981f1f4b996d1377ac89ded65ae1dc169
Alina Nesterova <zemuvier@gmail.com>
2014-11-21 00:22:54 +0300

    UPD

--- Graph/5a.cpp
+++ Graph/5a.cpp
@@ -19,21 +19,15 @@ vector <vector <int> > vec;
 
 void dfs(int m) //алгоритм обхода в глубину
 {
-	visit[m] = 1;
+visit[m] = 1;
 
-	for (int i = 0; i < vec[m-1].size(); i++)
-	if (visit[vec[m-1][i]] == 0)
-	{
-		visit[i] = 1;
-		dfs(vec[m-1][i]); 
-	}
+for (int i = 0; i < vec[m-1].size(); i++)
+if (visit[vec[m-1][i]] == 0)
+{
+visit[i] = 1;
+dfs(vec[m-1][i]); 
+}
 
-	for (int i = 0; i < m; i++)
-		if (visit[i] == 1)
-		{
-			cout << i << " ";
-			k++;
-		}
 }
 
 int main()
@@ -49,22 +43,29 @@ getline(inp,str);
 
 for (int i = 0; i < n; i++)
 {
-	getline(inp,str);
-	stringstream sio(str);
-	int x;
+getline(inp,str);
+stringstream sio(str);
+int x;
 
-	while (sio >> x)
-		vec[i].push_back(x);
+while (sio >> x)
+vec[i].push_back(x);
 }
-	dfs(n); 
+dfs(n); 
 
-	cout << k;
 
-	if (k == n)
-	cout << "YEP";
+for (int i = 0; i <= n; i++)
+if (visit[i] == 1)
+{
+cout << i << " ";
+k++;
+}
+cout << endl;
+cout << "k = " << k << " ";
+if (k == n)
+cout << "YEP";
 
-	else 
-		cout << "NOPE";
-
-	return 0;
+else 
+cout << "NOPE";
+system ("pause");
+return 0;
 }
